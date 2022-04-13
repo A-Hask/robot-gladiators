332e9285ef5647bd6d00cea8fe44a47979a7dfa5
Qu33nArlene <ahaskins93@yahoo.com>
2022-04-12 21:33:02 -0400

    randomized fight order

--- assets/js/game.js
+++ assets/js/game.js
@@ -43,20 +43,33 @@ var fightOrSkip = function () {
 
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
+  //keep track of who goes first
+  var isPlayerTurn = true;
+  if (Math.random() > 0.5) {
+    isPlayerTurn=false;
+  }
   while (playerInfo.health > 0 && enemy.health > 0) {
+    if (isPlayerTurn) {}
     //ask player if they'd like to fight or skip using fightOrSkip function
     if (fightOrSkip()) {
       //if true, leave fight by breaking loop    
     break;
       }
-    }
 
     // generate random damage value based on player's attack power
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
+    //remove enemy's health by subtracting the amount we set in the damage variable
     enemy.health = Math.max(0, enemy.health - damage);
     console.log(
-      playerInfo.name + ' attacked ' + enemy.name + '. ' + enemy.name + ' now has ' + enemy.health + ' health remaining.'
+      playerInfo.name +
+      ' attacked ' +
+      enemy.name +
+      '. ' +
+      enemy.name +
+      ' now has ' +
+      enemy.health +
+      ' health remaining.'
     );
 
     // check enemy's health
@@ -71,7 +84,8 @@ var fight = function(enemy) {
     } else {
       window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
     }
-
+    //player gets attacked first
+  }else {
     // remove players's health by subtracting the amount set in the enemy.attack variable
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
 
@@ -87,8 +101,11 @@ var fight = function(enemy) {
       // leave while() loop if player is dead
       break;
     } else {
-      window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
+        window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
+      }
     }
+    //switch turn order for next round
+    isPlayerTurn = !isPlayerTurn;
   }
 };
 
